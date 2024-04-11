e7b1d0e43d7d7f6f8d182e8435aebfbe43aecf03
MeongGanas <farreluken@gmail.com>
2024-04-11 22:44:36 +0800

    wrap useSearchParams in suspense

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,13 @@
 import { SigninForm } from "@/components/auth/AuthForm";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <main className="w-full min-h-screen p-5 flex items-center justify-center">
       <div className="min-w-96">
-        <SigninForm />
+        <Suspense>
+          <SigninForm />
+        </Suspense>
       </div>
     </main>
   );
